c4e07bf0d506af8a3d0792aabafd00e5aa7a884c
Dawid Śleziak <dawid.sleziak@gmail.com>
2023-10-19 18:03:59 +0200

    Implementing HTTP Requests to the server + respond to the home page

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+    res.send("<h1>Home page</h1>");
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
