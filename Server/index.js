ba81c01863428f855f7f0f956266bbd6896bda8c
Dawid Śleziak <dawid.sleziak@gmail.com>
2023-10-21 22:30:29 +0200

    Creating the loopback connection to the mysql database

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import mysql from "mysql2";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'pbw_base'
+}).promise();
 
 const app = express();
 const port = 3000;
@@ -10,9 +18,8 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/Client/index.html");
 });
 
+// Express server will start listening on the port in port variable
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
 
-// use node {fileName} to start listening on port
-// Ctrl+C -> exit the server listening state
