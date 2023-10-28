725a71c83939e2b93750420ce50ba3a6712d771c
Dawid Śleziak <dawid.sleziak@gmail.com>
2023-10-28 11:29:03 +0200

    Implementation of encoding password and change of login type with correctness check

--- Server/index.js
+++ Server/index.js
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import dotenv from 'dotenv';
+import bcrypt from 'bcrypt';
+import validator from 'email-validator';
 
 dotenv.config();
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -20,21 +22,61 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+async function checkLogin(providedLogin) {
+    const dbFetchLogin = await pool.query(`SELECT Login FROM Users`);
+    const logins = dbFetchLogin[0];
+    
+    for(const element of logins) {
+        if (element.Login == providedLogin) {
+            // The user with provided login is present in data base
+            console.log('Found!');
+            return false;
+        }
+    };
+
+    return true;
+}
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/Client/index.html");
 });
 
-app.post("/signup", (req, res) => {
-    console.log(req.body);
-    pool.query('INSERT INTO Users (Login, Password) VALUES (?,?)', [req.body.login,req.body.password]);
+app.post("/signup", async (req, res) => {
+    const loginCorrectness = validator.validate(req.body.login);
+    const duplicationCorrectness = await checkLogin(req.body.login);
+    const plainPassword = req.body.password;
+
+    console.log(loginCorrectness);
+    console.log(duplicationCorrectness);
+    // Check if the email address is valid and there is no other user with the same e-mail address
+    if (loginCorrectness && duplicationCorrectness) {
+        bcrypt.hash(plainPassword, 10, function(err, hash) {
+            pool.query('INSERT INTO Users (Login, Password) VALUES (?,?)', [req.body.login,hash]);
+        });
+    }
     res.redirect("/");
 });
 
 app.post("/signin", async (req, res) => {
-    const dbValues = await pool.query(`SELECT *
+    const dbValues = await pool.query(`SELECT 
+    Login
     FROM Users
-    WHERE login = ? AND password = ?`, [req.body.login,req.body.password]);
-    if (req.body.login == dbValues[0][0].Login && req.body.password == dbValues[0][0].Password) {
+    WHERE login = ?`, [req.body.login]);
+
+    console.log(dbValues);
+    // Check the authorization
+    let passwordValid = false;
+
+    bcrypt.compare(req.body.password, dbValues[0].Password, function(err, result) {
+        console.log(result);
+        if (result) {
+            console.log(result);
+            passwordValid = true;
+        }
+    })
+
+    console.log(`Login: ${dbValues.Login}`);
+    if (req.body.login == dbValues[0].Login && passwordValid) {
         res.redirect("/Client/main_page.html");
     } else {
         console.log('dane logowania sie nie zgadzaja');
@@ -51,4 +93,3 @@ app.get("/Client/main_page.html", (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
 });
-
