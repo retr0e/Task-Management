9f058dc63e77f0ab8ad21c0188acc7f1954296f9
Dawid Śleziak <dawid.sleziak@gmail.com>
2023-12-02 21:08:07 +0100

    setting up vite tailwindcss and react. entry phase of file refactor

--- Server/controllers/overviewControler.js
+++ Server/controllers/overviewControler.js
@@ -19,26 +19,26 @@ const pool = mysql
 
 export function showMain(req, res) {
   console.log(req.params);
-  let access;
-  switch (req.params.acs * 1) {
-    case 1:
-      access = "administratora";
-      break;
-    case 2:
-      access = "menadzera";
-      break;
-    case 3:
-      access = "pracownika";
-      break;
-    default:
-      access = "obserwatora";
-      break;
-  }
+  //   let access;
+  //   switch (req.params.acs * 1) {
+  //     case 1:
+  //       access = "administratora";
+  //       break;
+  //     case 2:
+  //       access = "menadzera";
+  //       break;
+  //     case 3:
+  //       access = "pracownika";
+  //       break;
+  //     default:
+  //       access = "obserwatora";
+  //       break;
+  //   }
 
-  res
-    .status(200)
-    .send(
-      `Witaj: ${req.params.usr}, twoj poziom uprawnien jest na poziomie ${access}`
-    );
-  //   res.sendFile(`${__dirname}/Client/main_page.html`);
+  //   res
+  //     .status(200)
+  //     .send(
+  //       `Witaj: ${req.params.usr}, twoj poziom uprawnien jest na poziomie ${access}`
+  //     );
+  res.sendFile(`${__dirname}/Client/main_page.html`);
 }
