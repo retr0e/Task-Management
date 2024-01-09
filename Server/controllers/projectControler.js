91349d9f2a96c9923a791fba42d6e0a461399aec
Dawid Śleziak <dawid.sleziak@gmail.com>
2024-01-09 13:49:47 +0100

    Cleaning up project controller

--- Server/controllers/projectControler.js
+++ Server/controllers/projectControler.js
@@ -1,41 +1,9 @@
-import mysql from "mysql2";
-import path from "path";
-import { fileURLToPath } from "url";
-
-import dotenv from "dotenv";
-dotenv.config({ path: "./config.env" });
-
-let __dirname = path.dirname(fileURLToPath(import.meta.url));
-__dirname = path.join(__dirname + "/..");
-
-const pool = mysql
-  .createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-  })
-  .promise();
+import { getAllProjectsData } from "./../model/projectModel.js";
 
 export async function getProjects(req, res, next) {
   try {
-    const projects = await pool.query(`
-    SELECT
-      p.ID,
-      p.Nazwa AS Nazwa_Projektu,
-      z.Nr_zespolu,
-      p.Id_priorytetu,
-      pr.Priorytety AS Priorytet,
-      p.Id_statusu,
-      s.Nazwa AS Status,
-      DATE_FORMAT(p.Data_start, '%Y-%m-%d') AS Data_start,
-      DATE_FORMAT(p.Data_koniec, '%Y-%m-%d') AS Data_koniec
-    FROM
-      Projekty p
-    JOIN Priorytety pr ON p.Id_priorytetu = pr.Id
-    JOIN Zespoly z ON p.Id_zespolu = z.Id
-    JOIN Status s ON p.Id_statusu = s.Id;`);
-    // console.log(projects);
+    const projects = await getAllProjectsData();
+
     res.status(200).json({
       result: projects[0],
     });
@@ -44,20 +12,3 @@ export async function getProjects(req, res, next) {
     next(err);
   }
 }
-
-
-/* old querry
-    SELECT
-      p.ID,
-      p.Nazwa AS Nazwa_Projektu,
-      z.Nr_zespolu,
-      pr.Priorytety AS Priorytet,
-      s.Nazwa AS Status,
-      DATE_FORMAT(p.Data_start, '%Y-%m-%d') AS Data_start,
-      DATE_FORMAT(p.Data_koniec, '%Y-%m-%d') AS Data_koniec
-    FROM
-      Projekty p
-    JOIN Priorytety pr ON p.Id_priorytetu = pr.Id
-    JOIN Zespoly z ON p.Id_zespolu = z.Id
-    JOIN Status s ON p.Id_statusu = s.Id; 
-*/
---
+++ Server/model/projectModel.js
@@ -0,0 +1,32 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config.env" });
+
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+export const getAllProjectsData = async () => {
+  return await pool.query(`
+    SELECT
+      p.ID,
+      p.Nazwa AS Nazwa_Projektu,
+      z.Nr_zespolu,
+      p.Id_priorytetu,
+      pr.Priorytety AS Priorytet,
+      p.Id_statusu,
+      s.Nazwa AS Status,
+      DATE_FORMAT(p.Data_start, '%Y-%m-%d') AS Data_start,
+      DATE_FORMAT(p.Data_koniec, '%Y-%m-%d') AS Data_koniec
+    FROM
+      Projekty p
+    JOIN Priorytety pr ON p.Id_priorytetu = pr.Id
+    JOIN Zespoly z ON p.Id_zespolu = z.Id
+    JOIN Status s ON p.Id_statusu = s.Id;`);
+};
