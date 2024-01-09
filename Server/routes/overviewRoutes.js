554d3990bca04a30fa9cd75cc4f5a9c0268ea2f4
Dawid Śleziak <dawid.sleziak@gmail.com>
2024-01-09 12:25:55 +0100

    Delete of obsolete route

--- Server/routes/overviewRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-import { showMain } from "../controllers/overviewControler.js";
-
-const router = express.Router();
-
-router.route("/:usr/:acs").get(showMain);
-
-export default router;
