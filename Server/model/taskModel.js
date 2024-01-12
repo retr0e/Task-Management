ceb9ea32d81bb76320f9369784108c8ba6a99670
Dawid Śleziak <dawid.sleziak@gmail.com>
2024-01-12 15:47:23 +0100

    Create task template

---
+++ Server/model/taskModel.js
@@ -0,0 +1,17 @@
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config.env" });
+
+const pool = mysql
+  .createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+export const addTask = async (data) => {
+  console.log("Przypał");
+};
