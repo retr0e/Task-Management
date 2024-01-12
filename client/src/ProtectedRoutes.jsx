ca5f85aeefbe69db7684555cf20ce19f8f14f088
PatrykSzymura <121195581+PatrykSzymura@users.noreply.github.com>
2024-01-12 23:10:28 +0100

    Checking Privilage level

--- client/src/ProtectedRoutes.jsx
+++ client/src/ProtectedRoutes.jsx
@@ -1,3 +1,4 @@
+import { data } from "autoprefixer";
 import { useState, useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
@@ -13,14 +14,15 @@ export const useAuth = () => {
             "Content-Type": "application/json",
           },
         });
+
         const data = await res.json();
 
-        // const res2 = await fetch("/api/v1/users/privilege", {
-        //   method: "GET",
-        //   headers: {
-        //     "Content-Type": "application/json",
-        //   },
-        // });
+         /* const res2 = await fetch("/api/v1/users/privilege", {
+           method: "GET",
+           headers: {
+             "Content-Type": "application/json",
+           },
+         }); */
         if (data.success) {
           setIsAuthenticated(true);
         } else {
@@ -38,11 +40,68 @@ export const useAuth = () => {
   return { loggedIn: isAuthenticated, setIsAuthenticated };
 };
 
+export const usePriv = () => {
+  const [isPrivilaged, setIsPrivilaged] = useState(false);
+  useEffect(() => {
+    const checkPrivilage = async() => {
+      try{
+        const response = await fetch("/api/v1/users/privilege", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const data = response.json();
+        data.result['privilage'];
+
+      }catch(error){
+        console.error("Authentication check error:", error);
+        setIsPrivilaged(false)
+      };
+    };
+    checkPrivilage();
+  },[]);
+  return {privilage: isPrivilaged, setIsPrivilaged};
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export const ProtectedRoutes = ({ isAuthenticated }) => {
   return isAuthenticated ? <Outlet /> : <Navigate to='/' />;
 };
 
-/* Teporarly USELESS */
-export const RestrictedRoutes = ({ isAuthenticated }) => {
-  return isAuthenticated ? <Outlet /> : <Navigate to='/' />;
-};
+
