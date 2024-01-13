10d875240ebe02e3028ef78052e93eb83c6c72b0
Dawid Śleziak <dawid.sleziak@gmail.com>
2024-01-13 16:40:02 +0100

    Setting up registeration based on privilege

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -32,7 +32,7 @@ export default function SignUp() {
       }
       setLoading(false);
       setError(null);
-      navigate("/sign-in");
+      // navigate("/sign-in");
     } catch (error) {
       setLoading(false);
       setError(error.message);
