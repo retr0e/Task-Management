435e6a19b313154fefe72ff1210bfff7db2edc56
PatrykSzymura <121195581+PatrykSzymura@users.noreply.github.com>
2024-01-14 17:48:01 +0100

    overview rework

--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 export default function About() {
-  return <div>Hello and Welcome this is Task Menager!</div>;
+  return (
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">Hello there</h1>
+          <p className="py-6">
+          We're thrilled to have you here at Task Manager, your go-to destination 
+          for efficient task management and productivity! 
+          </p>
+          <Link to='/sign-in'>
+              
+                <span className='btn btn-primary'>Sign In</span>
+              
+            </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
