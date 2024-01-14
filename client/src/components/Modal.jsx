741b378e7ce7db4b110fd69355aecf045b0d7ae1
Dawid Śleziak <dawid.sleziak@gmail.com>
2024-01-14 20:56:40 +0100

    Adding AccessBlock handling function

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -2,23 +2,36 @@ import React from "react";
 import Add_Project_Form from "../pages/Forms";
 import { Outlet } from "react-router-dom";
 
-;
-
-const Modal = ({ element , btn_Name , btn_Style }) => {
+const Modal = ({ element, btn_Name, btn_Style }) => {
   return (
     <>
-        <button className={btn_Style} onClick={()=>document.getElementById('my_modal_3').showModal()}>{btn_Name}</button>
-        <dialog id="my_modal_3" className="modal">
-        <div className="modal-box">
-        <form method="dialog">
+      <button
+        className={btn_Style}
+        onClick={() => document.getElementById("my_modal_3").showModal()}
+      >
+        {btn_Name}
+      </button>
+      <dialog id='my_modal_3' className='modal'>
+        <div className='modal-box'>
+          <form method='dialog'>
             {/* if there is a button in form, it will close the modal */}
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>      
-        </form>
-            {element}
+            <button className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2'>
+              ✕
+            </button>
+          </form>
+          {element}
         </div>
-        </dialog>
+      </dialog>
     </>
   );
+};
+
+const AccessBlock = ({ element, reqLvl, currLvl }) => {
+  if (currLvl <= reqLvl) {
+    return { element };
+  } else {
+    return "";
+  }
 };
 
 export default Modal;
