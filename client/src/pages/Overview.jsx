61165e8c39a4c2f72c99ae24d7eebe2a160a5668
PatrykSzymura <121195581+PatrykSzymura@users.noreply.github.com>
2024-01-16 00:24:39 +0100

    If u know u know

--- client/src/pages/Overview.jsx
+++ client/src/pages/Overview.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 export default function About() {
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content text-center">
+    <div className="hero min-h-screen bg-meme">
+      <div className="hero-content text-center strokeme2 bg-gray-800 card bg-opacity-50">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Hello there</h1>
           <p className="py-6">
