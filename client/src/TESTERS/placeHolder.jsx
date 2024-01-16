b50a277177f101d051c97048c25f6228bb612c75
PatrykSzymura <121195581+PatrykSzymura@users.noreply.github.com>
2024-01-16 20:04:02 +0100

    .

--- client/src/TESTERS/placeHolder.jsx
+++ client/src/TESTERS/placeHolder.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 
-import { Team_Form_X } from "../pages/Forms";
+import { DesChangeForm, Team_Form_X } from "../pages/Forms";
 import Profile from "../pages/Profile";
 import Test from "./test";
 import { AddProject, ProjectList } from "../pages/Home";
+import Modal from "../components/modal";
 
 const Component = () => {
-  return <Team_Form_X />;
+ 
+  return <Modal element={<DesChangeForm/>} btn_Name={'Name'}/>;
 };
 export default Component;
